700ed757993050512ec0897319d02483cc7272fd
Dkstormz14 <djk14@ymail.com>
2026-05-25 19:18:32 +0100

    Update Attack.cs

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -14,6 +14,8 @@ public class Attack : MonoBehaviour
 
         if (damageable != null)
         {
+            Vector2 deliveredKnockback = transform.parent.localScale.x > 0 ? knockback : new Vector2(-knockback.x * -1, knockback.y);
+
             // Hit the target
             bool gotHit = damageable.Hit(attackDamage, knockback);
 
