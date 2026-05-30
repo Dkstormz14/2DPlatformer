0dbfe6ec3778205423e89ebf671f307db8e3bf68
Dkstormz14 <djk14@ymail.com>
2026-05-30 20:55:23 +0100

    Sword Combo Done

--- Assets/Scripts/Knight.cs
+++ Assets/Scripts/Knight.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D), typeof(TouchingDirections), typeof(Damageable))]
 public class Knight : MonoBehaviour
 {
-    public float walkSpeed = 3f;
+    public float walkAcceleration = 3f;
+    public float maxSpeed = 3f;
     public float walkStopRate = 0.05f;
     public DetectionZone attackZone;
     public DetectionZone cliffDetectionZone;
@@ -104,7 +105,10 @@ public class Knight : MonoBehaviour
         if(!damageable.LockVelocity)
         {
             if (CanMove)
-                rb.linearVelocity = new Vector2(walkSpeed * WalkDirectionVector.x, rb.linearVelocity.y);
+                // Accelerate to max speed in the current walk direction
+            rb.linearVelocity = new Vector2(
+                Mathf.Clamp(rb.linearVelocity.x + (walkAcceleration * WalkDirectionVector.x * Time.fixedDeltaTime), -maxSpeed, maxSpeed),
+                rb.linearVelocity.y);
             else
                 rb.linearVelocity = new Vector2(Mathf.Lerp(rb.linearVelocity.x, 0, walkStopRate), rb.linearVelocity.y);
         }
